12842057a80d700c74d4ea81a7f077b10187f2fc
Jaradrj <jara.derooij@edu.tbz.ch>
2026-03-04 13:48:53 +0100

    add Compass

--- App.tsx
+++ App.tsx
@@ -1,45 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Magnetometer } from 'expo-sensors';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+export default function Compass() {
+    const [{ x, y, z }, setData] = useState({
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+    const [subscription, setSubscription] = useState(null);
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+    const _slow = () => Magnetometer.setUpdateInterval(1000);
+    const _fast = () => Magnetometer.setUpdateInterval(16);
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    const _subscribe = () => {
+        setSubscription(
+            Magnetometer.addListener(result => {
+                setData(result);
+            })
+        );
+    };
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Magnetometer:</Text>
+            <Text style={styles.text}>x: {x}</Text>
+            <Text style={styles.text}>y: {y}</Text>
+            <Text style={styles.text}>z: {z}</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+                    <Text>{subscription ? 'On' : 'Off'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+                    <Text>Slow</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={_fast} style={styles.button}>
+                    <Text>Fast</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+    },
+    text: {
+        textAlign: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        marginTop: 15,
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eee',
+        padding: 10,
+    },
+    middleButton: {
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: '#ccc',
+    },
 });
-
-export default App;
